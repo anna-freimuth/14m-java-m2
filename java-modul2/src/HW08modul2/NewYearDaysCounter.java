1bd47b4580ea46ea7331f050e4fedaa2a8aee1c1
Anna Freimuth <a.freimuth90@gmail.com>
2020-11-14 17:56:08 +0100

    Added homework 8.

---
+++ java-modul2/src/HW08modul2/NewYearDaysCounter.java
@@ -0,0 +1,26 @@
+package HW08modul2;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class NewYearDaysCounter {
+    public static void main(String[] args) {
+        System.out.println(NewYearDaysCount());
+    }
+
+    public static LocalDate getDateInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a date, from which you want to count days until next Mew Year (yyyy-MM-dd)");
+        String date = input.next();
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    public static int NewYearDaysCount() {
+        LocalDate newYear = LocalDate.of(2021, 1, 1);
+        Period period = Period.between(getDateInput(), newYear);
+        return period.getDays();
+    }
+}
+
